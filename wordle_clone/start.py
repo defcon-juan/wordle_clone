48197d1e3c737742deb742404027eb964ad7e6f6
Defcon-Juan <50335228+defcon-juan@users.noreply.github.com>
2023-10-15 21:42:43 +0100

    Delete wordle_clone directory

--- wordle_clone/start.py
+++
@@ -1,9 +0,0 @@
-# start.py
-
-for guess_num in range(1, 7):
-    guess = input("\nGuess a word: ").upper()
-    if guess == "SNAKE":
-        print("Correct")
-        break
-    else:
-        print("Wrong")
