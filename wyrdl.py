26e2b90382cf2ab845f70eab71c1b1b6f8b83b58
alex <adtaylor107@gmail.com>
2023-10-16 21:17:17 +0100

    basic function with limited wordlist

--- wyrdl.py
+++ wyrdl.py
@@ -1,8 +1,42 @@
 # wrdyl.py
 
+import pathlib
+import random
+
+WORDLIST = pathlib.Path("wordlist.txt")
+
+words = [
+    word.upper() for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
+]
+
+word = random.choice(words)
+
+
 for guess_num in range(1, 7):
     guess = input("Guess a word: ").upper()
-    if guess == "SNAKE":
+    if guess == word:
         print("Correct")
         break
-    print("Wrong")
+    # using set comprehension to organise letters
+    # newSet= { expression for element in  iterable if condition}
+
+    # Description of the syntax:
+
+    # The iterable may be any iterable object or data structure in Python from which we have to use the elements to create the new set.
+    # The condition is a conditional expression using
+    # The element denotes the element from the iterable that has to be included in the set.
+    # The expression can be any mathematical expression derived from the element.
+    # newSet is the name of the new set which has to be created from the elements of the iterable.
+
+    correct_letters = {
+        letter for letter, correct in zip(guess, word) if letter == correct
+    }
+
+    misplaced_letters = set(guess) & set(word) - correct_letters
+    wrong_letters = set(guess) - set(word)
+
+    print("Correct letters:", ", ".join(sorted(correct_letters)))
+    print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
+    print("Wrong letters:", ", ".join(sorted(wrong_letters)))
+else:
+    print(f"The word was {word}")
